b71707de356d08e06b2b00e4ea639d949204f0f9
harkamal <gajinder@zeam.in>
2025-02-07 12:31:59 +0530

    pass the dummy CI

--- build.zig
+++ build.zig
@@ -2,5 +2,6 @@ const Builder = @import("std").Build;
 
 // TODO build test monorepo
 pub fn build(b: *Builder) void {
-    _ = b;
+    const test_step = b.step("test", "Run unit tests");
+    _ = test_step;
 }
