34d402077860c787cacbaeef040748d52fdf850e
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-07-25 11:48:27 +0200

    zisk: fix get_input being optimized out
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- pkgs/state-transition-runtime/src/zisk/io.zig
+++ pkgs/state-transition-runtime/src/zisk/io.zig
@@ -1,14 +1,13 @@
 const std = @import("std");
 const mem_layout = @import("./layout.zig");
 
-var input_ptr: [*]const u8 = @ptrFromInt(mem_layout.INPUT_ADDR);
+var input_ptr: [*]const volatile u8 = @ptrFromInt(mem_layout.INPUT_ADDR);
 
 pub fn get_input(_: std.mem.Allocator) []const u8 {
-    const input_size = std.mem.readInt(u64, input_ptr[8..16], .little);
+    const input_size = std.mem.readInt(u64, @volatileCast(input_ptr[8..16]), .little);
     if (input_size > mem_layout.INPUT_SIZE) @panic("invalid input size");
-    return input_ptr[8 .. 8 + input_size];
-// note pour quand je rentre: il semblerait que get_input ne soit pas compile, ce qui pourrait avoir
-// a voir avec l'absence de volatile.
+    const non_volatile_input = @volatileCast(input_ptr[16 .. 16 + input_size]);
+    return non_volatile_input;
 }
 
 pub const zisk_archid = 0xFFFEEEE;
