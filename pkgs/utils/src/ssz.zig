2b655bf6e520322de18397df64fe4f08b6851d42
Chetany Bhardwaj <chetanyiot@gmail.com>
2026-01-26 02:04:15 +0530

    feat: add use_poseidon flag to enable poseidon based hash tree root hashing

--- pkgs/utils/src/ssz.zig
+++ pkgs/utils/src/ssz.zig
@@ -1,14 +1,28 @@
 const std = @import("std");
 const ssz = @import("ssz");
+const build_options = @import("build_options");
 
 const Allocator = std.mem.Allocator;
-const Sha256 = std.crypto.hash.sha2.Sha256;
+
+const Hasher = blk: {
+    if (build_options.use_poseidon) {
+        const hash_zig = @import("hash_zig");
+        break :blk hash_zig.ssz.SszHasher;
+    }
+    break :blk std.crypto.hash.sha2.Sha256;
+};
+
+comptime {
+    if (Hasher.digest_length != 32) {
+        @compileError("SSZ hasher must have 32-byte digest length");
+    }
+}
 
 pub fn hashTreeRoot(
     comptime T: type,
     value: T,
-    out: *[Sha256.digest_length]u8,
+    out: *[Hasher.digest_length]u8,
     allocator: Allocator,
 ) !void {
-    try ssz.hashTreeRoot(Sha256, T, value, out, allocator);
+    try ssz.hashTreeRoot(Hasher, T, value, out, allocator);
 }
